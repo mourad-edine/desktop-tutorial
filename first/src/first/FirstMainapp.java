297bdf0c35f5be9f2a53004ddc520955326821eb
mourad-edine <101376159+mourad-edine@users.noreply.github.com>
2023-01-28 09:22:43 +0300

    fa izy

---
+++ first/src/first/FirstMainapp.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package first;
+
+import com.sun.javafx.logging.PlatformLogger.Level;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author chams
+ */
+public class FirstMainapp {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //connect to database
+        try {
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/mourad","root","1234");
+            if(connect == null){
+                System.out.println("echec");
+            }else{
+                 System.out.println("ça marche");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+         
+
+    }
+    
+    
+}
